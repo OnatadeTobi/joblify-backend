52f97fa4c5cf92872b1aad18fd0bddc4b081382d
tobi <onatadetobi212@gmail.com>
2025-06-03 16:56:45 -0700

    fix for superuser

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,20 +3,37 @@
 echo "Running migrations..."
 python manage.py migrate
 
-echo "Creating superuser if it doesn't exist..."
-python manage.py shell -c "
-from django.contrib.auth import get_user_model
-User = get_user_model()
-username = '${DJANGO_SUPERUSER_USERNAME:-admin}'
-email = '${DJANGO_SUPERUSER_EMAIL:-admin@example.com}'
-password = '${DJANGO_SUPERUSER_PASSWORD:-adminpassword}'
-if not User.objects.filter(username=username).exists():
-    User.objects.create_superuser(username=username, email=email, password=password)
-"
+echo "Creating superuser..."
+python manage.py shell < scripts/createsuperuser.py
 
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
-
 echo "Starting server..."
 exec gunicorn JoblifyBackend.wsgi:application --bind 0.0.0.0:8000
+
+
+
+
+# #!/bin/sh
+
+# echo "Running migrations..."
+# python manage.py migrate
+
+# echo "Creating superuser if it doesn't exist..."
+# python manage.py shell -c "
+# from django.contrib.auth import get_user_model
+# User = get_user_model()
+# username = '${DJANGO_SUPERUSER_USERNAME:-admin}'
+# email = '${DJANGO_SUPERUSER_EMAIL:-admin@example.com}'
+# password = '${DJANGO_SUPERUSER_PASSWORD:-adminpassword}'
+# if not User.objects.filter(username=username).exists():
+#     User.objects.create_superuser(username=username, email=email, password=password)
+# "
+
+# echo "Collecting static files..."
+# python manage.py collectstatic --noinput
+
+
+# echo "Starting server..."
+# exec gunicorn JoblifyBackend.wsgi:application --bind 0.0.0.0:8000
