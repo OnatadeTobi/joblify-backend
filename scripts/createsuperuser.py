3e153b0480c021926e29175ae0687406fba02ce7
tobi <onatadetobi212@gmail.com>
2025-06-03 22:08:23 -0700

    fixing superuser v4

--- scripts/createsuperuser.py
+++ scripts/createsuperuser.py
@@ -4,22 +4,14 @@ from django.contrib.auth import get_user_model
 def run():
     User = get_user_model()
 
-    username = os.environ.get("DJANGO_SUPERUSER_USERNAME", "admin")
     email = os.environ.get("DJANGO_SUPERUSER_EMAIL", "admin@example.com")
     password = os.environ.get("DJANGO_SUPERUSER_PASSWORD", "adminpassword")
 
-    if not User.objects.filter(username=username).exists():
-        User.objects.create_superuser(username=username, email=email, password=password)
+    if not User.objects.filter(email=email).exists():
+        User.objects.create_superuser(email=email, password=password)
         print("Superuser created.")
-        print("Env username:", os.environ.get("DJANGO_SUPERUSER_USERNAME"))
-        print("Env email:", os.environ.get("DJANGO_SUPERUSER_EMAIL"))
-        print("Env password:", os.environ.get("DJANGO_SUPERUSER_PASSWORD"))
-
+        
     else:
         print("Superuser already exists.")
-        print("Env username:", os.environ.get("DJANGO_SUPERUSER_USERNAME"))
-        print("Env email:", os.environ.get("DJANGO_SUPERUSER_EMAIL"))
-        print("Env password:", os.environ.get("DJANGO_SUPERUSER_PASSWORD"))
-
-
+        
 run()
