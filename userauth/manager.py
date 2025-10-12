0e121f27174692c5697e24ea4bdd5b3ebf5ca65e
tobi <onatadetobi212@gmail.com>
2025-10-12 22:43:30 +0100

    streaks

--- userauth/manager.py
+++ userauth/manager.py
@@ -1,11 +1,11 @@
 from django.contrib.auth.models import BaseUserManager
-from django.core.exceptions import ValidationError
+from django.core.exceptions import ValidationError 
 from django.core.validators import validate_email
-from django.utils.translation import gettext_lazy as _
+from django.utils.translation import gettext_lazy as _ 
 
 class UserManager(BaseUserManager):
     def email_validator(self, email):
-        try:
+        try: 
             validate_email(email)
         except ValidationError:
             raise ValueError(_('Please enter a valid email address'))
