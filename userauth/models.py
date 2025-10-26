16f4bdf1c241e67821c7e2ef54271ab4481f4e46
tobi <onatadetobi212@gmail.com>
2025-10-26 23:39:55 +0100

    fftg

--- userauth/models.py
+++ userauth/models.py
@@ -13,7 +13,7 @@ class User(AbstractBaseUser ,PermissionsMixin):
     first_name = models.CharField(max_length=100, verbose_name=('First Name'))
     last_name = models.CharField(max_length=100, verbose_name=('Last Name'))
     public_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
-    is_staff = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)         
     is_superuser = models.BooleanField(default=False)
     is_verified = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
